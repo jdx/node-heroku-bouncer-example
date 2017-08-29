12f5c49d2c678406f2693481727d9b58835e6dd3
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-08-28 17:56:44 -0700

    finished basic example

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const app = require('express')()
 const session = require('express-session')
+const bouncer = require('heroku-bouncer')
 
 const production = process.env.NODE_ENV === 'production'
 
+// setup logging
+app.use(require('morgan')('dev'))
+
 app.use(session({
-  secret: 'keyboard cat',
+  secret: process.env.SECRET || 'keyboard cat',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: production }
 }))
+
+app.use(bouncer())
+
+// render a page with a login button
+app.get('/', (req, res) => {
+  console.dir(req.session)
+  res.render('home.ejs')
+})
 
 let port = process.env.PORT || 3000
 app.listen(port, () => {
